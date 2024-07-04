fec9552b2fc02612073afa4c7111d2ccb7777f70
Antonio Amate Carrión <aamatecarrion@gmail.com>
2024-07-04 21:37:57 +0200

    colores bien

--- src/components/Dia/Dia.jsx
+++ src/components/Dia/Dia.jsx
@@ -1,34 +1,45 @@
-import React, { useContext } from 'react';
-import { LocalStorageContext } from '../../contexts/LocalStorageContext';
-import formatDate from '../../utils/formatDate';
-import RegistrosDia from '../RegistrosDia/RegistrosDia';
+import React, { useContext } from "react";
+import { LocalStorageContext } from "../../contexts/LocalStorageContext";
+import formatDate from "../../utils/formatDate";
+import RegistrosDia from "../RegistrosDia/RegistrosDia";
+
+const diasSemana = [
+  "colorLunes",
+  "colorMartes",
+  "colorMiércoles",
+  "colorJueves",
+  "colorViernes",
+  "colorSábado",
+  "colorDomingo",
+];
 
 const getCustomDay = (date) => {
   const day = date.getDay();
-  return day === 0 ? 6 : day - 1;
+  return day === 0 ? 6 : day - 1; // Convertir domingo (0) a 6 y otros días ajustados a 0-5
 };
 
 const Dia = (props) => {
   const { data } = useContext(LocalStorageContext);
-  const registrosDia = data.regs.filter((reg) => formatDate(reg.date) === props.dia);
+  const registrosDia = data.regs.filter(
+    (reg) => formatDate(reg.date) === props.dia
+  );
   const numeroDiaSemana = getCustomDay(new Date(props.dia));
-  const color = data && data.colors && data.colors[numeroDiaSemana];
-  
-  
+  const colorKey = diasSemana[numeroDiaSemana];
+  const color = data && data.colors && data.colors[colorKey];
+
   return (
     <React.Fragment>
       <tr style={{ backgroundColor: color }}>
-        <td style={{ padding: '5px' }}>
-          {new Date(props.dia).toLocaleDateString('es-ES', { weekday: 'long' })}
+        <td style={{ padding: "5px" }}>
+          {new Date(props.dia).toLocaleDateString("es-ES", { weekday: "long" })}
         </td>
-        <td style={{ padding: '5px', textAlign: 'center' }}>{props.dia}</td>
-        <td style={{ padding: '5px' }}>{registrosDia.length}</td>
+        <td style={{ padding: "5px", textAlign: "center" }}>{props.dia}</td>
+        <td style={{ padding: "5px" }}>{registrosDia.length}</td>
       </tr>
-      
+
       <RegistrosDia registrosDia={registrosDia} color={color} />
     </React.Fragment>
   );
 };
 
 export default Dia;
-
