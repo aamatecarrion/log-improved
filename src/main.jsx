deffe0042a5e76d3ce972be7efeece2ba143cd30
aamatecarrion <aamatecarrion@gmail.com>
2024-07-21 13:38:31 +0200

    a ver si despliega

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { LocalStorageProvider } from './contexts/LocalStorageContext.jsx';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LocalStorageProvider>
       <App />
     </LocalStorageProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/log-improved/"
-})
+  base: "./",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Log v2 (Beta)',
+        short_name: 'Log',
+        description: 'Log v2 (Beta) como una Progressive Web App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
